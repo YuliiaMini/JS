b48ca6bf42e17900f4b7cd1dd486a8280a97407d
Yuliia Minienok <usyavk@ukr.net>
2022-06-21 19:57:57 +0300

    added new tasks

---
+++ JS.HW_2.1.js
@@ -0,0 +1,18 @@
+let age_1 = 10
+let age_2 = 18
+let age_3 = 60
+    const checkAge = function(age){
+        if (age <=age_1){
+            console.log("You don’t have access cause your age is ",age,"It’s less then ", age_2)
+        } else if(age >age_1 && age < age_2){
+            console.log("You don’t have access cause your age is ",age,"It’s less then ", age_2)
+        } else if(age >=  age_2 && age <  age_3){
+            console.log("Welcome  !")
+            } else if (age >= age_3){
+            console.log("Keep calm and look Culture channel")
+            }else{console.log("Technical work")
+            }
+    }
+    checkAge(17)
+    checkAge(18)
+    checkAge(61)
